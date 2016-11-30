30fa046035d323b59bd41c485ca64280ed6e9d37
Yeo Jun Yeong <yeogandalf@gmail.com>
2016-11-30 12:32:21 +0900

    modify >>Hello.java:0215238 by YJY

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -7,6 +7,9 @@ public class Hello {
 		// 012456 : hotfix edit by YJY
 		System.out.println("hotfix edit/modify");
 		// 012456 : end
+		
+		// 01215238:origin/master edit by YJY
+		System.out.println("GitHub edit");
 	}
 
 }
