782f8c1d7fa7ddfb917fc6656f487d9ccb34f72f
yjy <yeogandalf@gmail.com>
2016-11-30 11:50:26 +0900

    modify>>Hello.java:012456 by YJY

--- Hello_JSP/src/com/test/hello/Hello.java
+++ Hello_JSP/src/com/test/hello/Hello.java
@@ -4,7 +4,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("local Repository create Hello_jsp");
-
+		// 012456 : hotfix edit by YJY
+		System.out.println("hotfix edit/modify");
+		// 012456 : end
 	}
 
 }
